91157afba9efe6046b31414b522cf7d083911afb
Anton Deulia <anton.deulia06@gmail.com>
2026-05-08 16:13:12 +0200

    feat: msgs, srvs, actions created

--- aeroops_bringup/launch/display.launch.py
+++ aeroops_bringup/launch/display.launch.py
@@ -5,23 +5,30 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    uav_description_path = PathJoinSubstitution(
+    bringup_pkg = FindPackageShare("aeroops_bringup")
+
+    aeroops_description_path = PathJoinSubstitution(
         [
-            FindPackageShare("uav_description"),
+            FindPackageShare("aeroops_description"),
             "urdf",
-            "uav.urdf.xacro",
+            "aeroops.urdf.xacro",
         ]
     )
 
+    rviz2_config = PathJoinSubstitution(
+        [bringup_pkg, "rviz2", "config.rviz"]
+    )
+
     robot_description = {
-        "robot_description": Command(["xacro ", uav_description_path])
+        "robot_description": Command(
+            ["xacro ", aeroops_description_path]
+        )
     }
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
-        output="screen",
         parameters=[robot_description],
     )
 
@@ -30,7 +37,11 @@ def generate_launch_description():
         executable="joint_state_publisher_gui",
     )
 
-    rviz2 = Node(package="rviz2", executable="rviz2")
+    rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", rviz2_config],
+    )
 
     return LaunchDescription(
         [robot_state_publisher, joint_state_publisher_gui, rviz2]
